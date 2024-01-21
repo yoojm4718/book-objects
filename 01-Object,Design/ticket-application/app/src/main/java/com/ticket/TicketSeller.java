88a61584758ab5497246ea0ec718ebbbde9a8058
jungminyoo <yoojm4718@outlook.com>
2024-01-22 01:02:48 +0900

    01_ticket-application : improvement 2. buy method encapsulation

--- 01-Object,Design/ticket-application/app/src/main/java/com/ticket/TicketSeller.java
+++ 01-Object,Design/ticket-application/app/src/main/java/com/ticket/TicketSeller.java
@@ -7,16 +7,9 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
+    // Audience 클래스의 buy 메소드 선언을 통한 캡슙화
+    //  -> TicketSeller 클래스도 Bag의 존재를 알 필요가 없음
     public void sellTo(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
